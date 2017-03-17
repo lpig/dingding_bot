14cfbf0acde2e039448eff306b8fe2f102a04f50
lpig <o55662000@yeah.net>
2017-03-17 15:11:45 +0800

    add setup

--- dingding_bot/__init__.py
+++ dingding_bot/__init__.py
@@ -4,10 +4,12 @@ import sys
 from etc.dingding_logging import *
 from api.bot import DingDingBot
 
-__title__ = 'dingding_rot'
+__title__ = 'dingding_bot'
 __version__ = '0.0.1'
 __author__ = 'Lpig'
 __license__ = 'MIT'
 
 version_details = 'wxpy {ver} from {path} (python {pv.major}.{pv.minor}.{pv.micro})'.format(
     ver=__version__, path=__path__[0], pv=sys.version_info)
+
+VERSION = __version__
--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@
 import re
 from setuptools import setup, find_packages
 
-with open('dingding_bot') as fp:
+with open('dingding_bot/__init__.py') as fp:
     version = re.search(r"__version__\s*=\s*'([\w\-.]+)'", fp.read()).group(1)
 
 long_description = u'简易钉钉机器人'
-url = u'https://github.com/lpig/dingding_rot'
+url = u'https://github.com/lpig/dingding_bot'
 
-setup(name="wechat",
-      version=version,
-      description=long_description,
-      maintainer="Lpig",
-      maintainer_email="o55662000@yeah.net",
-      url=url,
-      long_description=long_description,
-      install_requires=['requests'],
-      packages=find_packages('.'),
-      )
+setup(
+    name="dingding-bot",
+    version=version,
+    description=long_description,
+    author='Lpig',
+    author_email='o55662000@yeah.net',
+    url=url,
+    long_description=long_description,
+    install_requires=['requests'],
+    packages=find_packages('.'),
+)
